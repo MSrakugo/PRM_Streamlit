861b0b1886aa9697a69f10774687950bbf1ed88f
Satoshi Matsuno <51268793+MSrakugo@users.noreply.github.com>
2023-11-22 03:28:44 -0500

    Citation change

--- Library_model_construction.py
+++ Library_model_construction.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-@author: satoshi_matsuno
+Begin: Tue Mar  1 23:06:08 2022
+Final update: 2023/11/21
+
+Author: 松野哲士 (Satoshi Matsuno), Tohoku university, Japan
+Contact: satoshi.matsuno.p2@dc.tohoku.ac.jp
+Citation: Matsuno, S., Uno, M., Okamoto, A. Tsuchiya, N. Machine-learning techniques for quantifying the protolith composition and mass transfer history of metabasalt. Sci Rep 12, 1385 (2022).
+App Citation: Satoshi Matsuno. (2023). Graphical interface to construct and apply Machine-learning based Protolith Reconstruction Models (PRMs) (v1.1). Zenodo. https://doi.org/10.5281/zenodo.10183974
 """
 
 #plot&calc
